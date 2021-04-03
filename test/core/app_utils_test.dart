5628345e427627668cc1d3ae6e581fff8eef1deb
Baraa Aljabban <Baraa>
2021-04-03 12:43:21 +0800

    Start with testing : test app utils : 3 success 1 fail

---
+++ test/core/app_utils_test.dart
@@ -0,0 +1,80 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:setel_geofanc/core/app_utils.dart';
+import 'package:setel_geofanc/error/failures.dart';
+
+main() {
+  InputConverter inputConverter;
+  setUp(() {
+    inputConverter = new InputConverter();
+  });
+
+  group(
+    "stringToUnsignedDouble2",
+    () {
+      test(
+        "should return an integer when the string represents an unsigned Double",
+        () async {
+          // arrange
+          final xPoint = "123";
+          final yPoint = "222";
+          //act
+          final result = inputConverter.stringToUnsignedDouble2(
+            xPoint: xPoint,
+            yPoint: yPoint,
+          );
+          //assert
+          expect(result, Right(Tuple2(123, 222)));
+        },
+      );
+    },
+  );
+
+  group(
+    "stringToUnsignedDouble",
+    () {
+      test(
+        "should return an integer when the string represents an unsigned Double",
+        () async {
+          // arrange
+          final xPoint = "123";
+          final yPoint = "222";
+          final radius = "333";
+
+          //act
+          final result = inputConverter.stringToUnsignedDouble(
+            xPoint: xPoint,
+            yPoint: yPoint,
+            radius: radius,
+          );
+          //assert
+          expect(result, Right(Tuple3(123, 222, 333)));
+        },
+      );
+    },
+  );
+
+    group(
+    "stringToUnsignedDouble",
+    () {
+      test(
+        "should return Left InvalidInputFailure",
+        () async {
+          // arrange
+          final xPoint = "-123";
+          final yPoint = "-333";
+          final radius = "444";
+
+          //act
+          final result = inputConverter.stringToUnsignedDouble(
+            xPoint: xPoint,
+            yPoint: yPoint,
+            radius: radius,
+          );
+          //assert
+          expect(result, Left(InvalidInputFailure("")));
+        },
+      );
+    },
+  );
+}
